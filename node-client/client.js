1a50723b1f39f5d9a4003df3362e9c82c8e5553a
gwerneckp <gwerneckpaiva@gmail.com>
2022-04-24 02:02:28 +0100

    Downgraded socketio and added role to node client.

--- node-client/client.js
+++ node-client/client.js
@@ -2,23 +2,27 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 //client.js
-var socket_io_client_1 = require("socket.io-client");
+const socket_io_client_1 = require("socket.io-client");
 //user input
-var prompt_sync_1 = __importDefault(require("prompt-sync"));
-var prompt = (0, prompt_sync_1["default"])();
+const prompt_sync_1 = __importDefault(require("prompt-sync"));
+const prompt = (0, prompt_sync_1.default)();
 // @ts-ignore 
 var socket = socket_io_client_1.io.connect('http://localhost:3000', { reconnection: true });
-//defining showboard function
-function showBoard(board) {
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
-    var boardPrint = "";
-    for (var i = board.length - 1; i > -1; i--) {
-        var line = "";
+let sessionId;
+socket.on('connect', function () {
+    sessionId = socket.id;
+});
+//defining showboardwhite function
+function showBoardWhite(board) {
+    const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    let boardPrint = "";
+    for (let i = board.length - 1; i > -1; i--) {
+        let line = "";
         boardPrint += "" + (i + 1) + " ";
         boardPrint += "  ";
-        for (var j = 0; j < board[i].length; j++) {
+        for (let j = 0; j < board[i].length; j++) {
             line += board[i][j].consoleColor + board[i][j].notation + '\x1b[0m';
             line += " ";
         }
@@ -27,18 +31,55 @@ function showBoard(board) {
     }
     boardPrint += "\n";
     boardPrint += "    ";
-    for (var k = 0; k < 8; k++) {
+    for (let k = 0; k < 8; k++) {
         boardPrint += letters[k];
         boardPrint += " ";
     }
     return boardPrint;
+}
+//defining showboardblack function
+function showBoardBlack(board) {
+    const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    let boardPrint = "";
+    for (let i = 0; i < board.length; i++) {
+        let line = "";
+        boardPrint += "" + (i + 1) + " ";
+        boardPrint += "  ";
+        for (let j = board[i].length - 1; j > -1; j--) {
+            line += board[i][j].consoleColor + board[i][j].notation + '\x1b[0m';
+            line += " ";
+        }
+        boardPrint += line;
+        boardPrint += "\n";
+    }
+    boardPrint += "\n";
+    boardPrint += "    ";
+    for (let k = 7; k > -1; k--) {
+        boardPrint += letters[k];
+        boardPrint += " ";
+    }
+    return boardPrint;
+}
+function showBoard(players, chess) {
+    if (sessionId == players.whitePlayer) {
+        return showBoardWhite(chess.board);
+    }
+    if (sessionId == players.blackPlayer) {
+        return showBoardBlack(chess.board);
+    }
+    if (chess.turn[0] == "white") {
+        return showBoardWhite(chess.board);
+    }
+    if (chess.turn[0] == "black") {
+        return showBoardBlack(chess.board);
+    }
 }
 // Add a connect listener
 socket.on('connect', function (socket) {
     console.log('Connected!');
 });
 function showTurn(turn) {
-    var turnColor;
+    let turnColor;
     if (turn[0] == "white") {
         turnColor = "\x1b[37m";
     }
@@ -48,25 +89,41 @@ function showTurn(turn) {
     return (turnColor + turn[0] + "'s turn" + "\x1b[0m");
 }
 function letterToNumber(ltr) {
-    var letters = { "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7 };
+    const letters = { "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7 };
     return letters[ltr];
 }
 function notationToNumbers(moveStr) {
-    var moveArr = [];
-    var arr = moveStr.split(" ");
+    let moveArr = [];
+    let arr = moveStr.split(" ");
     moveArr[0] = letterToNumber(arr[0][0]);
     moveArr[1] = parseInt(arr[0][1]) - 1;
     moveArr[2] = letterToNumber(arr[1][0]);
     moveArr[3] = parseInt(arr[1][1]) - 1;
     return moveArr;
 }
-socket.on("game", function (chess) {
-    var move;
+function rightTurn(players, chess) {
+    if (chess.turn[0] == "white") {
+        if (sessionId == players.whitePlayer) {
+            return true;
+        }
+    }
+    if (chess.turn[0] == "black") {
+        if (sessionId == players.blackPlayer) {
+            return true;
+        }
+    }
+    return false;
+}
+socket.on("game", function (players, chess) {
+    let move;
     console.log("\n");
-    console.log(showBoard(chess.board));
+    console.log(showBoard(players, chess));
     console.log("\n");
     console.log(showTurn(chess.turn));
-    var moveStr = prompt("next move: ");
-    move = notationToNumbers(moveStr);
-    socket.emit("game", move[0], move[1], move[2], move[3]);
+    if (rightTurn(players, chess)) {
+        const moveStr = prompt("next move: ");
+        move = notationToNumbers(moveStr);
+        console.log(move);
+        socket.emit("game", move[0], move[1], move[2], move[3]);
+    }
 });
--- node-client/client.ts
+++ node-client/client.ts
@@ -7,9 +7,13 @@ const prompt = promptSync();
 
 // @ts-ignore 
 var socket = io.connect('http://localhost:3000', {reconnection: true});
+let sessionId:string
+socket.on('connect', function(){
+  sessionId = socket.id
+})
 
-//defining showboard function
-function showBoard(board):string{
+//defining showboardwhite function
+function showBoardWhite(board):string{
     const letters:Array<string> = ["a","b","c","d","e","f","g","h"]
     let boardPrint:string = ""
     for(let i=board.length-1;i>-1;i--){
@@ -35,7 +39,51 @@ function showBoard(board):string{
       boardPrint += " "
     }
     return boardPrint
+}
+
+//defining showboardblack function
+function showBoardBlack(board):string{
+  const letters:Array<string> = ["a","b","c","d","e","f","g","h"]
+  let boardPrint:string = ""
+  for(let i=0;i<board.length;i++){
+
+    let line:string = ""
+
+    boardPrint += ""+(i+1)+" "
+    boardPrint += "  "
+
+    for(let j = board[i].length-1; j>-1; j--){
+      line += board[i][j].consoleColor+board[i][j].notation+'\x1b[0m'
+      line += " "
+    }
+    boardPrint += line
+    boardPrint += "\n"
+
   }
+
+  boardPrint += "\n"
+  boardPrint += "    "
+  for(let k=7;k>-1;k--){
+    boardPrint += letters[k]
+    boardPrint += " "
+  }
+  return boardPrint
+}
+
+function showBoard(players, chess):string{
+  if(sessionId == players.whitePlayer){
+    return showBoardWhite(chess.board)
+  }
+  if(sessionId == players.blackPlayer){
+    return showBoardBlack(chess.board)
+  }
+  if(chess.turn[0] == "white"){
+    return showBoardWhite(chess.board)
+  }
+  if(chess.turn[0] == "black"){
+    return showBoardBlack(chess.board)
+  }
+}
 
 // Add a connect listener
 socket.on('connect', function (socket: Socket) {
@@ -68,13 +116,30 @@ function notationToNumbers(moveStr):Array<number>{
   return moveArr
 }
 
-socket.on("game", function (chess) {
+function rightTurn(players, chess):boolean{
+  if(chess.turn[0] == "white"){
+      if(sessionId == players.whitePlayer){
+          return true
+      }
+  }
+  if(chess.turn[0] == "black"){
+      if(sessionId == players.blackPlayer){
+          return true
+      }
+  }
+  return false
+}
+
+socket.on("game", function (players, chess){
     let move:Array<number>
     console.log("\n")
-    console.log(showBoard(chess.board))
+    console.log(showBoard(players, chess))
     console.log("\n")
     console.log(showTurn(chess.turn))
-    const moveStr:string = prompt("next move: ")
-    move = notationToNumbers(moveStr)
-    socket.emit("game", move[0], move[1], move[2], move[3])
+    if(rightTurn(players,chess)){
+      const moveStr:string = prompt("next move: ")
+      move = notationToNumbers(moveStr)
+      console.log(move)
+      socket.emit("game", move[0], move[1], move[2], move[3])
+    }
 });
